014f9dd489f64783e98a3c5046bb3029aa879b71
asep1122a <130075061+asep1122a@users.noreply.github.com>
2025-05-03 14:19:35 +0700

    Customer.java

--- src/main/java/app/customer.java
+++ src/main/java/app/customer.java
@@ -1,14 +1,14 @@
 package app;
 
-public class customer {
+public class Customer {
     private String id;
     private String name;
-    private String email;
+    private String phoneNumber; // Mengganti email dengan phoneNumber
 
-    public customer() {}
-    public customer(String name, String email) {
+    public Customer() {}
+    public Customer(String name, String phoneNumber) { // Mengubah parameter constructor
         this.name = name;
-        this.email = email;
+        this.phoneNumber = phoneNumber; // Mengubah parameter constructor
     }
 
     public String getId() { return id; }
@@ -17,6 +17,6 @@ public class customer {
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getPhoneNumber() { return phoneNumber; } // Mengubah getter
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; } // Mengubah setter
 }
